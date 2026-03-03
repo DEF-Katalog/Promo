8aa46d054e605ff4dc32e3faa836d26d146c3888
DEF-Katalog <def.bandungjuara@gmail.com>
2026-03-03 19:11:52 +0700

    Update AdminUI.js

--- admin/AdminUI.js
+++ admin/AdminUI.js
@@ -113,4 +113,24 @@ showErrorBelow(elementId, message) {
 clearErrors() {
   document.querySelectorAll(".error").forEach(e => e.remove());
 },
+
+formatRupiah(value) {
+  const numberString = value.replace(/[^,\d]/g, "").toString();
+  const split = numberString.split(",");
+  const sisa = split[0].length % 3;
+  let rupiah = split[0].substr(0, sisa);
+  const ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+  if (ribuan) {
+    const separator = sisa ? "." : "";
+    rupiah += separator + ribuan.join(".");
+  }
+
+  return rupiah ? "Rp " + rupiah : "";
+},
+
+cleanNumber(value) {
+  return value.replace(/[^0-9]/g, "");
+},
+
 };
