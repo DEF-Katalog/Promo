3e763cff8ed96ff47ab92ae95033e828b94f7ae1
DEF-Katalog <def.bandungjuara@gmail.com>
2026-03-03 13:41:55 +0700

    Update ProductService.js

--- services/ProductService.js
+++ services/ProductService.js
@@ -1,1 +1,12 @@
+import { db } from "../firebase.js";
+import { ref, push, set } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
+export class ProductService {
+
+  static async saveProduct(product) {
+    const productRef = push(ref(db, "products"));
+    await set(productRef, product);
+    return productRef.key;
+  }
+
+}
